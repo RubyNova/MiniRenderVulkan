32b52401789bcb3ed77ea015a68af2630c739bef
Matt Jones <matthewj866@gmail.com>
2020-07-16 22:22:12 +0100

    Now working with correct texture.

--- app/src/main/cpp/VulkanCubePipeline/include/FileLoadingService.h
+++ app/src/main/cpp/VulkanCubePipeline/include/FileLoadingService.h
@@ -26,31 +26,5 @@ public:
 
 };
 
-inline int android_read(void* cookie, char* buf, int size) {
-        return AAsset_read((AAsset*)cookie, buf, size);
-}
-
-inline int android_write(void* cookie, const char* buf, int size) {
-        return EACCES; // can't provide write access to the apk
-}
-
-inline fpos_t android_seek(void* cookie, fpos_t offset, int whence) {
-        return AAsset_seek((AAsset*)cookie, offset, whence);
-}
-
-inline int android_close(void* cookie) {
-        AAsset_close((AAsset*)cookie);
-        return 0;
-}
-
-inline FILE* android_fopen(android_app* androidAppCtx, const char* fname, const char* mode) {
-        if(mode[0] == 'w') return NULL;
-
-        AAsset* asset = AAssetManager_open(androidAppCtx->activity->assetManager, fname, 0);
-        if(!asset) return NULL;
-
-        return funopen(asset, android_read, android_write, android_seek, android_close);
-}
-
 
 #endif //!FILELOADINGSERVICE_H
